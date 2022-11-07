fe401795de6fd7629dd96249cc22c5f94a8cb578
vaibhav-sharma-oss <80534369+iamvaibhav31@users.noreply.github.com>
2022-11-08 00:07:02 +0530

    Update in the basic setup

--- src/features/Slice/AccesTokenSlice.js
+++ src/features/Slice/AccesTokenSlice.js
@@ -4,25 +4,18 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
      LonglifeAccesstoken: "",
-     shortlifeAccesstoken: "",
      code: "",
      status: 'idle', // idle | loading | successful | failed
      error: null
 }
 
-const DriverSlice = createSlice({
-     name: 'Driver',
+const AccessTokenSlice = createSlice({
+     name: 'InstaAccessCode',
      initialState,
      reducers: {
-          // addDriver(state, actions) {
-          //      state.DriverData.name = actions.payload.name
-          //      state.DriverData.Role = actions.payload.role
-          //      state.DriverData.Currentlocation = actions.payload.currentlocation
-          //      state.DriverData.CurrentLongitude = actions.payload.CurrentLongitude
-          //      state.DriverData.CurrentLatitude = actions.payload.CurrentLatitude
+          // addInstaCode(state, actions) {
+
           // },
-
-
      },
      // extraReducers(builder) {
      //      builder
@@ -31,13 +24,7 @@ const DriverSlice = createSlice({
      //           })
      //           .addCase(registeDriver.fulfilled, (state, action) => {
      //                console.log(action.payload)
-     //                state.status = "successful"
-     //                state.DriverID = action.payload._id
-     //                state.DriverData.name = action.payload.name
-     //                state.DriverData.Role = action.payload.name
-     //                state.DriverData.Currentlocation = action.payload.name
-     //                state.DriverData.CurrentLongitude = action.payload.name
-     //                state.DriverData.CurrentLatitude = action.payload.name
+
      //           })
      //           .addCase(registeDriver.rejected, (state, action) => {
      //                state.status = "failed"
@@ -50,14 +37,14 @@ const DriverSlice = createSlice({
 })
 
 
-// export const { addPosts, addreactions, editPost } = DriverSlice.actions;
-
-
-export const getdriverid = (state) => state.Driver.DriverID
-export const getstatus = (state) => state.Driver.status
-export const geterror = (state) => state.Driver.error
+// export const { addInstaCode } = AccessTokenSlice.actions;
 
 
 
+// export const getinstacode = (state) => state.AccessToken.code
+// export const geterror = (state) => state.AccessToken.error
 
-export default DriverSlice.reducer;
+
+
+
+export default AccessTokenSlice.reducer;
--- src/features/store.js
+++ src/features/store.js
@@ -1,10 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
-
+import AccesTokenSlice from "./Slice/AccesTokenSlice";
 
 
 export const store = configureStore({
      reducer: {
-
+          AccessToken: AccesTokenSlice
 
      }
 });
