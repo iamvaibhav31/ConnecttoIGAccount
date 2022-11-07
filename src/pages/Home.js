c4c7e2c8861a6086c3498533a67b327ce1d7e2bc
vaibhav-sharma-oss <80534369+iamvaibhav31@users.noreply.github.com>
2022-11-08 01:20:47 +0530

    Removing the error in basic setup

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,20 +1,17 @@
 import React from "react";
-import { useSearchParams, Link } from "react-router-dom";
+import { useSearchParams } from "react-router-dom";
 import Insta from '../assets/svg/instagram.svg'
-import { InstaCodeGenratorURL } from '../utils/URLgenrator'
-// "start": "set HTTPS=true&&react-scripts start",
+
 const Home = () => {
           const [searchParams, setSearchParams] = useSearchParams();
           const code = searchParams.get("code")
 
           return (
                     <div className="h-screen  w-screen flex justify-center items-center ">
-
-                              {code ? <p className="text-xl font-mono sm:text-2xl underline cursor-pointer text-sky-800 hover:text-sky-600"> go to Profile view page </p> : <a href='https://api.instagram.com/oauth/authorize?client_id=1776611746023170&redirect_uri=https://localhost:3000/&scope=user_profile,user_media&response_type=code' className="flex justify-center outline  outline-pink-500 rounded  m-4 p-2 drop-shadow-md" >
+                              {code ? <p className="text-xl font-mono sm:text-2xl underline cursor-pointer text-sky-800 hover:text-sky-600"> go to Profile view page </p> : <a href='https://api.instagram.com/oauth/authorize?client_id=1776611746023170&redirect_uri=https://connecttoinstaaccount.netlify.app&scope=user_profile,user_media&response_type=code' className="flex justify-center outline  outline-pink-500 rounded  m-4 p-2 drop-shadow-md" >
                                         <img className="my-2" src={Insta} alt="icon" width={"12%"} />
                                         <p className="text-xl font-mono my-4 px-2 text-pink-500 cursor-pointer  sm:my-8" >connect to Instagram account</p>
                               </a>}
-
                     </div>
           );
 };
